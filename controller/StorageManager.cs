08350e619ffafabc54575b0191f8e58442f16caf
khaliiid1 <ac154133@avcol.school.nz>
2026-06-25 10:21:46 +1200

    i coded more and added comments to the databse in storageManager on what each section if code does.

---
+++ controller/StorageManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjuryLogs.controller
+{
+    Public class StorageManager
+
+    {
+        private SqlConnection conn;
+        public StorageManager(string connectionString)
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                Console.WriteLine("Connection Successful"); // if successfull this is the massage that will be displayed.
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Invalid connection string or connection already open.");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine($"SQL Error: {e.Message}");
+                Console.WriteLine($"SQL Error: {e.Message}");
+                if (e.Message.Contains("attach an auto-named database")) // these are the possible fixes that could help the databse run correctly.
+                {
+                    Console.WriteLine("Fix: Database is already attached OR file is in use.");
+                    Console.WriteLine("Try this:");
+                    Console.WriteLine("1. Remove AttachDbFilename from connection string");
+                    Console.WriteLine("2. Use Initial Catalog instead");
+                    Console.WriteLine("3. Or delete/rename duplicate DB in SQL Server");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to database: {ex.Message}");  // if there is an error this is the message that will be displayed.
+            }
