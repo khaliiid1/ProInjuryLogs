74902ecb38552230b19d2adc908cbdc560767ec9
khaliiid1 <ac154133@avcol.school.nz>
2026-07-20 18:40:09 +1200

    started working on my console view

--- View/ConsoleViewcs.cs
+++ View/ConsoleViewcs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProInjuryLogs.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,28 @@ using System.Threading.Tasks;
 
 namespace InjuryLogs.View
 {
-    internal class ConsoleViewcs
+    public class ConsoleView
     {
-    }
+        public void DisplayBrandMenu()
+        {
+            Console.WriteLine("Injury Menu:");
+            Console.WriteLine("1. View all records in Injury table");
+            Console.WriteLine("2. Update a injury's name by injury_id");
+            Console.WriteLine("3. Insert a new brand");
+            Console.WriteLine("4. Delete a injury by injury_name");
+            Console.WriteLine("5. Exit");
+            Console.Write("Select an option: ");
+        }
+        public void DisplayBrands(List<Injury> brandsList)
+        {
+            foreach (Injuries brandsObject in brandsList)
+            {
+                Console.WriteLine($"{brandsObject.InjuryID}, {brandsObject.InjuryName}");
+            }
+        }
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public string GetInput()
 }
